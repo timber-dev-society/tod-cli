3fb5414909981fd89bd1b7ef81f6a1a502bdff06
Nicolas Dufreche <ndufreche@clever-age.com>
2021-05-24 10:33:39 +0200

    changing listing style

--- src/commands/ls.js
+++ src/commands/ls.js
@@ -1,15 +1,18 @@
 const chalk = require('chalk')
+const Table = require('cli-table')
 
 const { getRelativeTime } = require('../core/time')
+const { print } = require('../core')
 const { connect } = require('../store')
 
 const command = 'ls'
 
-const parseTask = ({ uid, description, done, created }) => {
-  const state = `[${chalk.green(done ? 'x' : ' ')}]`
-  const identifier = `#${uid.substring(0, 5)}`
+const parseTask = (task) => {
+  print(task)
+  const state = `[${chalk.green(task.done ? 'x' : ' ')}]`
+  const identifier = `#${task.uid.substring(0, 5)}`
   
-  return `${state} ${identifier} ${description}`
+  return `${state} ${identifier} ${task.pssssasdescription}`
 }
 
 const parseBacklog = ({ uid, description, created }) => {
@@ -18,21 +21,39 @@ const parseBacklog = ({ uid, description, created }) => {
   const age = `${relativeTime.duration}${relativeTime.type.charAt(0)}`
   const identifier = `#${uid.substring(0, 5)}`
   
-  return `${age} ${identifier} ${description}`
+  return [ identifier, age, description ]
 }
 
-const stateToProps = ({ tasks, backlogs }) => ({
+const renderBacklogs = (backlogs) => {
+  print('====== Backlogs ======')
+
+  const table = new Table({ head: ['UID', 'Age', 'Description'] })
+  backlogs.forEach(backlog => table.push(parseBacklog(backlog)))
+
+  print(table.toString())
+}
+
+const renderTasks = (tasks) => {
+  print('====== Tasks =====')
+
+  const table = new Table({ head: [' ', 'UID', 'Description'] })
+  tasks.forEach(task => table.push(parseTask(task)))
+
+  print(table.toString())
+}
+
+const stateToProps = ({ tasks, backlogs, app }) => ({
+  context: app.context,
   tasks,
   backlogs,
 })
 
 const action = connect(
   stateToProps,
-)(() => ({ tasks, backlogs }) => {
-  console.log('Backlogs ======')
-  backlogs.forEach(backlog => console.log(parseBacklog(backlog)))
-  console.log('Tasks =====')
-  tasks.forEach(task => console.log(parseTask(task)))
+)(() => ({ tasks, backlogs, context }) => {
+  backlogs.length !== 0 && renderBacklogs(backlogs)
+
+  tasks[context] !== undefined && tasks[context].length !== 0 && renderTasks(tasks[context])
 })
 
 module.exports = {
