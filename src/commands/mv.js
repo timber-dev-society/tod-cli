9e81d65e4123cf083675b9e72797e95b49b361cc
Nicolas Dufreche <ndufreche@clever-age.com>
2021-06-11 19:22:34 +0200

    add backlog option to MV cmd

--- src/commands/mv.js
+++ src/commands/mv.js
@@ -8,32 +8,45 @@ const command = 'mv <identifier>'
 
 const stateToProps = ({ backlogs, app }) => ({
   context: app.context,
+  todos: app.todo[context],
   backlogs,
 })
 
 const mapDispatchToProps = dispatch => ({
-  moveTodo: (context, todo) => {
+  moveToTodo: (context, todo) => {
     dispatch(deleteBacklog(todo.uid))
     dispatch(addTodo(context, todo))
+  },
+  moveToBacklog: () => {
+    dispatch(deleteTodo(context, todo.uid))
+    dispatch(addBacklog(todo))
   },
 })
 
 const action = connect(
   stateToProps,
   mapDispatchToProps
-)(uid => ({ context, backlogs, moveTodo }) => {
+)((uid, { backlog }) => ({ context, backlogs, todos, moveToTodo, moveToBacklog }) => {
   const uidMatcher = buildUidMatcher(uid)
-  const todo = backlogs.find(backlog => uidMatcher.test(backlog.uid))
+  if (backlog) {
+    const todo = todos.find(todo => uidMatcher.test(todo.uid))
 
+    moveToBacklog(todo)
+  }
+
+  const todo = backlogs.find(backlog => uidMatcher.test(backlog.uid))
 
   if (todo === undefined) {
     throw 'Error todo not found in backlog'
   }
 
-  moveTodo(context, todo)
+  moveToTodo(context, todo)
 })
+
+const options = [{ option: '-b, --backlog', description: 'Move ToDo to backlogs'}]
 
 module.exports = {
   command,
   action,
+  options,
 }
